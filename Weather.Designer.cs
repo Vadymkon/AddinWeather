7267e0739a20f4f4119fc0bea8e40645a7b88bf3
Vadymkon <vadykakonon@gmail.com>
2024-10-25 10:29:40 +0300

    Add project files.

---
+++ Weather.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace TestAddinWeather
+{
+    partial class Weather : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Weather()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.button1 = this.Factory.CreateRibbonButton();
+            this.group2 = this.Factory.CreateRibbonGroup();
+            this.button2 = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.group2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Groups.Add(this.group2);
+            this.tab1.Label = "Vadymkon-testtask";
+            this.tab1.Name = "tab1";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.button1);
+            this.group1.Label = "CreateWeather";
+            this.group1.Name = "group1";
+            // 
+            // button1
+            // 
+            this.button1.Label = "button1";
+            this.button1.Name = "button1";
+            this.button1.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button1_Click);
+            // 
+            // group2
+            // 
+            this.group2.Items.Add(this.button2);
+            this.group2.Label = "Cripto";
+            this.group2.Name = "group2";
+            // 
+            // button2
+            // 
+            this.button2.Label = "button2";
+            this.button2.Name = "button2";
+            this.button2.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button2_Click);
+            // 
+            // Weather
+            // 
+            this.Name = "Weather";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Weather_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.group2.ResumeLayout(false);
+            this.group2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button2;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Weather Weather
+        {
+            get { return this.GetRibbon<Weather>(); }
+        }
+    }
+}
---
+++ Weather.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Excel = Microsoft.Office.Interop.Excel;
+using RestSharp;
+using Newtonsoft.Json.Linq;
+using System.Xml.Linq;
+using System.Threading.Tasks;
+
+namespace TestAddinWeather
+{
+    public partial class Weather
+    {
+        private readonly string apiKey = "dcccfefbd932d021573b9a94645ac4f5";
+        private readonly string apiKeyCrypto = "CF4415CB-E39E-4B31-94D1-732434BC56CB";
+        private readonly string baseUrlWeather = "http://api.openweathermap.org/data/2.5/forecast";
+        private readonly string baseUrlCrypto = "https://rest.coinapi.io/v1/trades/BITSTAMP_SPOT_BTC_USD/history";
+
+        public async Task<JObject> GetCryptoDataAsync()
+        {
+            var client = new RestClient();
+            var request = new RestRequest(baseUrlCrypto, Method.Get);
+            request.AddParameter("date", "2020-07-10");
+            request.AddHeader("Accept", "text/plain");
+            request.AddHeader("X-CoinAPI-Key", apiKey);
+            RestResponse response = await client.ExecuteAsync(request);
+
+            string result = response.Content;
+
+            return JObject.Parse(result);
+        }
+
+        public void InsertCryptoDataToExcel(Excel.Worksheet worksheet, JObject cryptoData)
+        {
+            worksheet.Cells[1, 1] = "Currency";
+            worksheet.Cells[1, 2] = "Rate";
+
+            var currency = cryptoData["asset_id_base"].ToString();
+            var rate = cryptoData["rate"].ToObject<double>();
+
+            worksheet.Cells[2, 1] = currency;
+            worksheet.Cells[2, 2] = rate;
+        }
+
+        public async Task<JArray> GetWeatherDataAsync(double lat, double lon)
+        {
+            var client = new RestClient(baseUrlWeather);
+            var request = new RestRequest();
+
+            request.AddParameter("lat", lat);
+            request.AddParameter("lon", lon);
+            request.AddParameter("appid", apiKey);
+            request.AddParameter("units", "metric");
+
+            var response = await client.ExecuteAsync(request);
+            string result = response.Content;
+
+            var json = JObject.Parse(result);
+            return (JArray)json["list"];
+        }
+
+        public void InsertWeatherDataToExcel(Excel.Worksheet worksheet, JArray weatherData)
+        {
+            int startRow = 2;
+            worksheet.Cells[1, 1] = "Date";
+            worksheet.Cells[1, 2] = "Temperature (°C)";
+            worksheet.Cells[1, 3] = "Feels Like (°C)";
+            worksheet.Cells[1, 4] = "Weather Description";
+
+            foreach (var dataPoint in weatherData)
+            {
+                var date = dataPoint["dt_txt"].ToString();
+                var temp = dataPoint["main"]["temp"].ToObject<double>();
+                var feelsLike = dataPoint["main"]["feels_like"].ToObject<double>();
+                var description = dataPoint["weather"][0]["description"].ToString();
+
+                worksheet.Cells[startRow, 1] = date;
+                worksheet.Cells[startRow, 2] = temp;
+                worksheet.Cells[startRow, 3] = feelsLike;
+                worksheet.Cells[startRow, 4] = description;
+                startRow++;
+            }
+
+            if (startRow > 2)
+            {
+                if (worksheet.AutoFilter != null)
+                {
+                    worksheet.AutoFilterMode = false;
+                }
+
+                Excel.Range usedRange = worksheet.UsedRange;
+                usedRange.AutoFilter(1);
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("No data to filter.");
+            }
+        }
+
+        private void Weather_Load(object sender, RibbonUIEventArgs e)
+        {
+        }
+
+        private async void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            Excel.Worksheet activeWorkSheet = Globals.ThisAddIn.Application.ActiveSheet as Excel.Worksheet;
+
+            if (activeWorkSheet != null)
+            {
+                double latitude = 50.4501;
+                double longitude = 30.5234;
+
+                var weatherData = await GetWeatherDataAsync(latitude, longitude);
+
+                InsertWeatherDataToExcel(activeWorkSheet, weatherData);
+            }
+        }
+
+        private async void button2_Click(object sender, RibbonControlEventArgs e)
+        {
+            Excel.Worksheet activeWorkSheet = Globals.ThisAddIn.Application.ActiveSheet as Excel.Worksheet;
+
+            if (activeWorkSheet != null)
+            {
+                var cryptoData = await GetCryptoDataAsync();
+
+                InsertCryptoDataToExcel(activeWorkSheet, cryptoData);
+            }
+        }
+    }
+}
